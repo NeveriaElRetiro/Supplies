b38e9c95380cdb76db482bd514b31ab723d3e0b3
Juan Carlos Ortiz Pinto <is672043@iteso.mx>
2021-06-08 23:44:28 -0500

    start project

---
+++ app/src/main/java/mx/neveriaelretiro/supplies/MainActivity.kt
@@ -0,0 +1,20 @@
+package mx.neveriaelretiro.supplies
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import mx.neveriaelretiro.supplies.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        Thread.sleep(2000)
+        setTheme(R.style.Theme_AppCompat_NoActionBar)
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fabSendList.setOnClickListener { Toast.makeText(this,"Worked with binding",Toast.LENGTH_SHORT).show() }
+    }
+}
